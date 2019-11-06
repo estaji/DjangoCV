8594450ed8ec267f2fe60c190df0ebac93346105
estaji <estaji.work@gmail.com>
2019-11-06 11:19:19 +0330

    Add url field to Job class and home.html

--- resume/models.py
+++ resume/models.py
@@ -4,6 +4,7 @@ class Job(models.Model):
     title = models.CharField(max_length=30)
     logo = models.ImageField(upload_to='logos/')
     company = models.CharField(max_length=30)
+    url = models.URLField(max_length=20, default='http://#')
     start = models.CharField(max_length=10)
     end = models.CharField(max_length=10)
     location = models.CharField(max_length=10)
